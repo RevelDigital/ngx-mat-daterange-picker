31a00b702a3846b91a2c79b004191efa9a7c20bd
Avery knight <averjk@gmail.com>
2023-05-22 13:52:05 -0500

    update to 15 complete

--- src/app/modules/ngx-mat-drp/ngx-mat-drp.module.ts
+++ src/app/modules/ngx-mat-drp/ngx-mat-drp.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NgxMatDrpComponent } from './ngx-mat-drp/ngx-mat-drp.component';
 import { PickerOverlayComponent } from './picker-overlay/picker-overlay.component';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 import { OverlayModule } from '@angular/cdk/overlay';
 
 import { CalendarWrapperComponent } from './calendar-wrapper/calendar-wrapper.component';
